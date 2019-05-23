c54b66ff59174ff384078e465f5701214ec4bb91
difficultForName <31028871+difficultForName@users.noreply.github.com>
2019-05-23 10:18:25 +0800

    (+) 过滤器的初步使用

---
+++ springcloud-zuul/src/main/java/com/yp/mina/springcloudzuul/filter/MyFilter.java
@@ -0,0 +1,55 @@
+package com.yp.mina.springcloudzuul.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Component
+public class MyFilter extends ZuulFilter {
+
+    private static Logger log = LoggerFactory.getLogger(MyFilter.class);
+
+    @Override
+    public String filterType() {
+        System.out.println("过滤器类型");
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        System.out.println("过滤器执行顺序-3");
+        return -3;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return false;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        log.info(String.format("%s >>> %s", request.getMethod(), request.getRequestURL().toString()));
+        Object accessToken = request.getParameter("token");
+        if(accessToken == null) {
+            log.warn("token is empty");
+            ctx.setSendZuulResponse(false);
+            ctx.setResponseStatusCode(401);
+            try {
+                ctx.getResponse().getWriter().write("token is empty");
+            }catch (Exception e){}
+
+            return null;
+        }else{
+            log.info("error");
+            return null;
+        }
+
+    }
+}
